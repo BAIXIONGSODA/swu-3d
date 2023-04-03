9d867e26cb8c954636c49733fe5f4bb88c1963c7
BAIXIONGSODA <wang.coder@outlook.com>
2023-04-03 15:34:27 +0800

    test示例完整实现-背景+3d模型+控制器

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="canvasContainer" ref="canvasContainer">
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { onMounted, onUnmounted, ref } from "vue";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const canvasContainer = ref(null);
+
+onMounted(() => {
+  // 初始化场景
+  const scene = new THREE.Scene();
+
+  // 初始化相机
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(1.5, 1, 1.5);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // 更新相机投影矩阵
+  camera.updateProjectionMatrix();
+
+  // 加载背景图片
+  const loader = new THREE.TextureLoader();
+  const texture = loader.load('/assets/images/sky.jpg');
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+
+  scene.background = texture;
+  scene.environment = texture;
+
+  // 加载模型
+  const gltfLoader = new GLTFLoader();
+  gltfLoader.load('/assets/models/scene.gltf', (gltf) => {
+    let model = gltf.scene;
+    console.log(model);
+    // 设置模型的缩放
+    scene.add(model);
+  });
+
+  // 初始化渲染器
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  // 设置渲染器的大小
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 监听窗口大小变化
+  window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  });
+
+  // 将渲染器的dom元素添加到页面中
+  canvasContainer.value.appendChild(renderer.domElement);
+
+  // 添加控制器
+  const controls = new OrbitControls(camera, renderer.domElement);
+  // 设置控制器阻尼
+  controls.enableDamping = true;
+
+
+  // 渲染函数
+  const render = () => {
+    // 更新控制器
+    controls.update();
+    // 渲染场景
+    renderer.render(scene, camera);
+    // 循环渲染
+    requestAnimationFrame(render);
+  };
+
+  // 开始渲染
+  render();
+
+
+});
+
+onUnmounted(() => {
+  
+});
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.canvasContainer {
+  margin: 0;
+  padding: 0;
 }
 </style>
